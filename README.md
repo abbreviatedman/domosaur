3ec0c4fee6402c1869bada42adff682ff8f9b91a
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-31 19:44:08 -0400

    finishes first four challenges

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Indomitable
+
+
+### Steps
+
+* change some text's font size
+* change the text's color
+* change the text's background color
+* change an image's size
+* change a div to be visibility: hidden
+* change a div to be flex-direction: row instead of column
---
+++ main.js
@@ -0,0 +1,19 @@
+// Change the span with the class `mess-with-me` to have a font size of 3em.
+const biggieSpans = document.querySelector('span.mess-with-me');
+biggieSpans.style.fontSize = '3em';
+
+
+// Change the span with the class `mess-with-me-more` to have the text be colored orange.
+const orangeSpan = document.querySelector('span.mess-with-me-more');
+orangeSpan.style.color = 'orange';
+
+
+// Change the paragraph with the class `mess-with-me` to have a background color of light green.
+const paragraph = document.querySelector('p.mess-with-me');
+paragraph.style.backgroundColor = 'lightgreen';
+
+
+// Change the dinosaur image's dimensions to be 324 pixels wide and 216 pixels tall.
+const dino = document.querySelector('img');
+dino.style.width = '324px';
+dino.style.height = '216px'
