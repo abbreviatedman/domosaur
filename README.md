a50d94b54ad953882d58b10ea917445eaca8ef01
Colin Jaffe <balloonasaurus@gmail.com>
2019-08-01 09:59:20 -0400

    adds more challenges still

--- README.md
+++ README.md
@@ -9,4 +9,5 @@
 * change an image's size
 * change a div to be visibility: hidden
 * change a div to be flex-direction: row instead of column
-* On click of a button, change a something to something else.b
+* On click of a button, change a something to something else.
+* On click of the same button, toggle it back.
--- main.js
+++ main.js
@@ -23,4 +23,27 @@ const hideMe = document.querySelector('#hide-me-area');
 hideMe.style.display = 'none';
 
 
-// 
+// Change the four images in their own `section` to be in a row. Check out the css file to see how this could be easily done!
+const dinosaurContainer = document.querySelector('#make-me-a-row');
+dinosaurContainer.style.flexDirection = 'row';
+
+// Change that row so that the images are _vertically_ centered within their container. If you put the DOM element in a variable, you shouldn't have to query it again!
+dinosaurContainer.style.alignItems = 'center';
+
+// Align the second image in the row to the bottom of the container. Feel free to edit the html to make selecting it easier, but you _can_ select it as is!
+const secondImage = document.querySelector('#bottom-aligned'); // Alternate selector: 'img:nth-child(2)'
+secondImage.style.alignSelf = 'flex-end';
+
+// Give the "Switch Background Color" button an event listener that launches a function to switch the background color of the row of dinosaurs. You don't have to write the function yet, but give it a name!
+const toggleButton = document.querySelector('#toggle');
+toggleButton.onclick = toggleBackgroundColor;
+
+// Now make the function you're going to call. Within the function, simply ask it to alert or console out something so we can see that it works.
+function toggleBackgroundColor() {
+
+  dinosaurContainer.style.backgroundColor = 'black';
+}
+
+// Now back in that function, remove the `alert`/`console.log`, and tell it to change the background color of the dinosaur row to black. If you made the dinosaur row container a variable, you should still have access to it there--in every code block, code can still look "up" to see any variable declared in an outer scope! So no need to query again.
+
+// STRETCH GOAL: Allow the button to switch the background color back to white. You can track which state it's currently in with a variable, but remember that at the end of any function or code block, any variable declared within is "garbage collected" and no longer exists in memory. You don't want to lose the state of the background's current color (or you might switch black to... black), so you'll have to store it somewhere OUTSIDE the function. ALTERNATELY, you could 
